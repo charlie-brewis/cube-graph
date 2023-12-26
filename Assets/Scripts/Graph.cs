f6d9a7f96efcd6d2cb864c6409b00e5b6a40b79e
Charlie Brewis <charliebrewis7@gmail.com>
2023-12-26 17:38:34 +0000

    Animated graph as sine wave

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -9,6 +9,9 @@ public class Graph : MonoBehaviour {
     [SerializeField, Range(10, 100)]
     int resolution = 10;
 
+    // Type transform is the position, rotation, and scale of the game object
+    Transform[] points;
+
     void Awake() { 
         // Transform point = Instantiate(pointPrefab);
         // // This method transforms the Vector3 position by (1, 0, 0)
@@ -23,18 +26,33 @@ public class Graph : MonoBehaviour {
         var scale = Vector3.one * xStep;
         // Initialise the position as (0, 0, 0)
         var position = Vector3.zero;
-        for (int i = 0; i < resolution; i++) {
+
+        // Objects like arrays must be initialized
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++) {
             // C# can tell this is inside a loop and so this definition is allowed
             Transform point = Instantiate(pointPrefab);
+            // Adding the point to the array of points
+            points[i] = point;
             position.x = (i + 0.5f) * xStep - 1f;
-            // y = f(x)
-            position.y = position.x * position.x * position.x;
 
             point.localPosition = position;
             // rescales the points to fit within -1-1 range
             point.localScale = scale;
 
+            //! I dont know what this is
             point.SetParent(transform, false);
+        }
+    }
+
+    void Update() {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            point.localPosition = position;
         }
     }
 }
