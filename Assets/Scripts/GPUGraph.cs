3f0b20d81bed2d2a032d30a1b2c68e964f901f51
Charlie Brewis <charliebrewis7@gmail.com>
2024-01-06 01:17:31 +0000

    Added compute shader with basic wave function

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -1,6 +1,9 @@
 using UnityEngine;
 
 public class GPUGraph : MonoBehaviour {
+    
+    [SerializeField]
+    ComputeShader computeShader;
 
     [SerializeField, Range(10, 200)]
     int resolution;
@@ -21,6 +24,21 @@ public class GPUGraph : MonoBehaviour {
 
     bool transitioning;
     FunctionLibrary.FunctionName transitionFunctionFrom;
+
+    // used to allocate GPU space for the positions
+    ComputeBuffer positionsBuffer;
+
+    // We use OnEnable instead of Awake as computeBuffers do not survive hot reloads, therefore we must call it each time the object is enabled
+    void OnEnable() {
+        // Constructor method of the compute buffer
+        // param 2 is the size of each datum to be stored, we are storing Vector3 objects which are sets of 3 floats (floats are 4 bytes)
+        positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+    }
+
+    void OnDisable() {
+        positionsBuffer.Release(); 
+        // As we wont use this instance again, we release it manually instead of waiting for the garbage collector
+        positionsBuffer = null; }
 
     void Update() {
         currDuration += Time.deltaTime;
